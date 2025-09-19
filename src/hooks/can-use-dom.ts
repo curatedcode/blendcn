862605c3fea09daf98084fbfcc91930925e0c910
Zack <me@zackaryf.com>
2025-09-19 08:43:23 -0400

    chore: stop biome trying to lint canUseDom variable

--- src/hooks/can-use-dom.ts
+++ src/hooks/can-use-dom.ts
@@ -1,3 +1,4 @@
+// biome-ignore-all lint/complexity/useOptionalChain: Breaks build if we switch to an optional chain. We need to be sure window exists.
 export const canUseDOM = !!(
 	typeof window !== "undefined" &&
 	window.document &&
