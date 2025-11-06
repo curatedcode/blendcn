7863ccd748d56f82aadde4985ebc63e6e65d0d2f
Zack <me@zackaryf.com>
2025-11-06 18:23:15 -0500

    chore: removed unused console.log

--- src/components/component-preview/toast-preview.tsx
+++ src/components/component-preview/toast-preview.tsx
@@ -16,7 +16,7 @@ export function ToastPreview() {
 							description: "Sunday, December 03, 2023 at 9:00 AM",
 							action: {
 								label: "Undo",
-								onClick: () => console.log("Undo"),
+								onClick: () => {},
 							},
 						})
 					}
