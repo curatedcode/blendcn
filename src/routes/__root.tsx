eb0a6e0894e8354c5fdc1c16b27fa6008978088f
Zack <me@zackaryf.com>
2025-11-06 13:46:58 -0500

    feat: add meta tags for wider device support

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -18,8 +18,17 @@ export const Route = createRootRoute({
 				charSet: "utf-8",
 			},
 			{
+				name: "keywords",
+				content:
+					"radix, shadcn, theme builder, design tokens, ui system, palette editor, semantic mapping, tailwind, component preview, color system,",
+			},
+			{
 				name: "viewport",
-				content: "width=device-width, initial-scale=1",
+				content: "width=device-width, initial-scale=1, viewport-fit=cover",
+			},
+			{
+				"http-equiv": "X-UA-Compatible",
+				content: "IE=edge",
 			},
 			{
 				title: "BlendCN",
@@ -42,6 +51,10 @@ export const Route = createRootRoute({
 				content: "index, follow",
 			},
 			{
+				name: "theme-color",
+				content: "#E5E7EB",
+			},
+			{
 				name: "mobile-web-app-capable",
 				content: "yes",
 			},
@@ -58,45 +71,65 @@ export const Route = createRootRoute({
 				content: "default",
 			},
 			{
-				name: "og:title",
+				property: "og:title",
 				content: "BlendCN",
 			},
 			{
-				name: "og:description",
+				property: "og:description",
 				content:
 					"A semantic theme builder that maps Radix color steps to shadcn/ui tokens with live component previews, customizable palettes, and export-ready design tokens.",
 			},
 			{
-				name: "og:url",
+				property: "og:url",
 				content: "https://blendcn.zackaryf.com",
 			},
 			{
-				name: "og:site_name",
+				property: "og:site_name",
 				content: "BlendCN",
 			},
 			{
-				name: "og:locale",
+				property: "og:locale",
 				content: "en_US",
 			},
 			{
-				name: "og:image",
-				content: "https://blendcn.zackaryf.com/assets/icons/twitter-card.png",
+				property: "og:image",
+				content: "https://blendcn.zackaryf.com/assets/meta/og-card.png",
 			},
 			{
-				name: "og:image:width",
+				property: "og:image:secure_url",
+				content: "https://blendcn.zackaryf.com/assets/meta/og-card.png",
+			},
+			{
+				property: "og:image:width",
 				content: "1200",
 			},
 			{
-				name: "og:image:height",
+				property: "og:image:height",
 				content: "630",
 			},
 			{
-				name: "og:type",
+				property: "og:image:alt",
+				content: "BlendCN preview",
+			},
+			{
+				property: "og:updated_time",
+				content: new Date().toISOString(),
+			},
+			{
+				property: "og:type",
 				content: "website",
+			},
+			{
+				property: "og:ttl",
+				content: "600",
 			},
 			{
 				name: "twitter:card",
 				content: "summary_large_image",
+			},
+			{
+				name: "twitter:site",
+				content: "@IAmNotZack",
 			},
 			{
 				name: "twitter:site:id",
@@ -119,6 +152,46 @@ export const Route = createRootRoute({
 				name: "twitter:image",
 				content: "https://blendcn.zackaryf.com/assets/icons/twitter-card.png",
 			},
+			{
+				name: "twitter:image:alt",
+				content: "BlendCN preview",
+			},
+			{
+				name: "msapplication-starturl",
+				content: "/?utm_source=homescreen",
+			},
+			{
+				name: "msapplication-navbutton-color",
+				content: "#E5E7EB",
+			},
+			{
+				name: "application-name",
+				content: "BlendCN",
+			},
+			{
+				name: "color-scheme",
+				content: "light dark",
+			},
+			{
+				name: "format-detection",
+				content: "telephone=no",
+			},
+			{
+				name: "msapplication-TileImage",
+				content: "/assets/icons/144x144.png",
+			},
+			{
+				name: "msapplication-TileColor",
+				content: "#E5E7EB",
+			},
+			{
+				name: "apple-touch-startup-image",
+				content: "/assets/icons/512x512.png",
+			},
+			{
+				name: "application-short-name",
+				content: "BlendCN",
+			},
 		],
 		links: [
 			{
@@ -131,42 +204,112 @@ export const Route = createRootRoute({
 			},
 			{
 				rel: "manifest",
-				href: "/manifest.json",
+				href: "/assets/meta/manifest.json",
 			},
 			{
 				rel: "canonical",
-				href: "/",
+				href: "https://blendcn.zackaryf.com/",
+			},
+
+			{
+				rel: "icon",
+				type: "image/x-icon",
+				href: "/favicon.ico",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "16x16",
+				href: "/assets/icons/16x16.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/assets/icons/32x32.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "48x48",
+				href: "/assets/icons/48x48.png",
+			},
+			{
+				rel: "icon",
+				sizes: "64x64",
+				type: "image/png",
+				href: "/assets/icons/64x64.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "192x192",
+				href: "/assets/icons/192x192.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "256x256",
+				href: "/assets/icons/256x256.png",
 			},
 			{
 				rel: "icon",
 				type: "image/svg+xml",
-				url: "/assets/icons/favicon.svg",
+				href: "/assets/icons/icon.svg",
 			},
 			{
-				rel: "icon",
-				sizes: "32x32",
-				type: "image/png",
-				url: "/assets/icons/favicon-32x32.png",
+				rel: "apple-touch-icon",
+				sizes: "60x60",
+				href: "/assets/icons/60x60.png",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "76x76",
+				href: "/assets/icons/76x76.png",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "114x114",
+				href: "/assets/icons/114x114.png",
 			},
 			{
 				rel: "apple-touch-icon",
 				sizes: "120x120",
-				url: "/assets/icons/apple-touch-icon-120x120.png",
+				href: "/assets/icons/120x120.png",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "144x144",
+				href: "/assets/icons/144x144.png",
 			},
 			{
 				rel: "apple-touch-icon",
 				sizes: "152x152",
-				url: "/assets/icons/apple-touch-icon-152x152.png",
+				href: "/assets/icons/152x152.png",
 			},
 			{
 				rel: "apple-touch-icon",
 				sizes: "180x180",
-				url: "/assets/icons/apple-touch-icon-180x180.png",
+				href: "/assets/icons/180x180.png",
 			},
 			{
 				rel: "apple-touch-icon",
 				sizes: "512x512",
-				url: "/assets/icons/apple-touch-icon-512x512.png",
+				href: "/assets/icons/512x512.png",
+			},
+			{
+				rel: "mask-icon",
+				href: "/assets/icons/icon-maskable.svg",
+				color: "#231f20",
+			},
+		],
+		scripts: [
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				name: "BlendCN",
+				url: "https://blendcn.zackaryf.com/",
+				type: "application/ld+json",
 			},
 		],
 	}),
@@ -176,10 +319,11 @@ export const Route = createRootRoute({
 function RootLayout() {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<head>
+			<head prefix="og: https://ogp.me/ns#">
 				<HeadContent />
 			</head>
 			<body className="font-body">
+				<Scripts />
 				<ThemeProvider
 					enableSystem
 					attribute={"class"}
