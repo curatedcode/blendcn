ab3492b68553a417f3b5227e08375d9e21eb0d4a
Zack <me@zackaryf.com>
2025-11-19 18:05:47 -0500

    fix: projects section icons not visible in dark mode

--- src/components/demo-app/projects-section.tsx
+++ src/components/demo-app/projects-section.tsx
@@ -25,14 +25,14 @@ export function ProjectsSection({
 				{projects.map((project, index) => (
 					<div key={project.title} className="flex gap-3">
 						<div
-							className="relative size-11 overflow-hidden rounded-md opacity-70"
+							className="relative size-11 overflow-hidden rounded-md opacity-70 dark:opacity-80"
 							style={{
 								// backgroundImage: `linear-gradient(to bottom, color-mix(in oklab, ${project.color} 70%, transparent 100%), ${project.color})`,
 								backgroundColor: project.color,
 							}}
 						>
 							<SparkleIcon
-								className="absolute size-14 fill-card stroke-transparent"
+								className="absolute size-14 fill-card stroke-transparent dark:stroke-[0.5] dark:stroke-white/50"
 								style={{
 									rotate: `${Math.floor(Math.random() * 44) * index}deg`,
 									left: `${index * 2}px`,
